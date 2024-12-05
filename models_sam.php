ccbb83eabdbd9af1d81a359da72365a453559cd9
Sandun Senarath <139609887+SandunSenarath@users.noreply.github.com>
2024-12-05 17:32:41 +0000

    modify models_sam page nav bar

--- models_sam.php
+++ models_sam.php
@@ -36,10 +36,10 @@
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto">
                   <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
+                    <a class="nav-link" href="about.php">About</a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link" href="#">Brand</a>
