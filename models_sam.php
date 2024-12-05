d302d820d72cb3f1a2f03b6572c35cbad71b5540
Sandun Senarath <139609887+SandunSenarath@users.noreply.github.com>
2024-12-05 17:22:13 +0000

    add samsung models page

---
+++ models_sam.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Samsung</title>
+    <link rel="stylesheet" href="CSS/New.css">
+	<link href="bootstrap-5.3.3-dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="head-c">
+	<div class="row">
+    <script src="bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
+    <header>
+      <div class="header">
+        <div class="container">
+        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+          <div class="full">
+              <div class="center-desk">
+                  <div class="logo">
+                      <h2><b>The Mobile Phone</b></h2>
+                  </div>
+              </div>
+          </div>
+        </div>
+
+        <!--navbar-->
+        <div class="nav1">
+          <!--<nav class="navbar navbar-expand-lg bg-body-tertiary">-->
+            <nav class="navbar navbar-expand-lg custom-navbar">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#"></a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Brand</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact Us</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="singup page.php">Sing Up</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Models
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="models_sam.php">Samsung</a></li>
+                      <li><a class="dropdown-item" href="models_iphone.php">iPhone</a></li>
+                      <!--<li><hr class="dropdown-divider"></li>-->
+                      <li><a class="dropdown-item" href="models_redmi.php">Redmi</a></li>
+                    </ul>
+                  </li>
+                </ul>
+                <form class="d-flex" role="search">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+              </div>
+            </div>
+          </nav>
+          </div>
+         </div>
+
+    <br>
+    <hr>
+</div>
+<h4>Samsung</h4>
+
+<!-- Products Group-->
+<div class="row-products">
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="products card h-100">
+      <img src="Img/samsung_S24.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Samsung S24</h5>
+        <p class="card-text">Display: 6.1" Dynamic AMOLED 2X, 120Hz, FHD+<br>
+          Chipset: Snapdragon 8 Gen 2 (USA/China) or Exynos 2200 (Global)<br>
+          Storage: 128 GB or 256 GB (no microSD)<br>
+          Cameras:<br>
+          Rear: 50 MP (main), 12 MP (ultrawide), 10 MP (telephoto, 3x zoom)<br>
+          Front: 12 MP<br>
+          Battery: 3,900 mAh, 25W fast charging<br>
+          Other: IP68 water resistance, 5G, no headphone jack, stereo speakers<br>
+          <br>
+          Rs.175000.00</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="products card h-100">
+      <img src="Img/samsung_S22n.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Samsung S22</h5>
+        <p class="card-text">Display: 6.1" Dynamic AMOLED 2X, 120Hz, FHD+<br>
+          Chipset: Snapdragon 8 Gen 1 (USA/China) or Exynos 2200 (Global)<br>
+          Storage: 128 GB or 256 GB (no microSD)<br>
+          Cameras:<br>
+          Rear: 50 MP (main), 12 MP (ultrawide), 10 MP (telephoto, 3x zoom)<br>
+          Front: 10 MP<br>
+          Battery: 3,700 mAh, 25W fast charging<br>
+          Other: IP68 water resistance, 5G, stereo speakers, no headphone jack<br><br>
+          Rs.139000.00</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="products card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="products card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="products card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="products card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="row-products">
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="products card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="products card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="products card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="products card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="products card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="products card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+</body>
+</html>
