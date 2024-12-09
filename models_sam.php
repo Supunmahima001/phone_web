5e269f6d57e09d66362cd2f29eadc063a5060bca
Sachinthani <139526328+Sachinthathani@users.noreply.github.com>
2024-12-09 18:29:30 +0000

    cards created for each products details

--- models_sam.php
+++ models_sam.php
@@ -67,7 +67,7 @@
                   <button class="btn btn-outline-success" type="submit">Search</button>
                 </form>
               </div>
-            </div>
+</div>
 </nav>
           </div>
          </div>
