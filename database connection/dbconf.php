b64565c798ed1d28e0588d0ca2d95b4b8f0621fb
SachinthaniManthilaka <139526328+SachinthaniManthilaka@users.noreply.github.com>
2024-12-10 12:04:14 +0000

    connection file created

---
+++ database connection/dbconf.php
@@ -0,0 +1,23 @@
+<?php
+//constant variable
+define('SERVERNAME', '127.0.0.1');
+define('USERNAME', 'root');
+define('PASSWORD', 'mariadb');
+define('DBNAME', 'mobile web');
+try {
+	//connect with database
+	$connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
+	if (!$connect) {
+		die("connection failed".mysqli_connect_error());
+	} 
+	else {
+		//echo "Connected successfully";
+	}
+} 
+catch (Exception $e) {
+	die($e->getMessage());
+}
+
+//echo "abc<br>";
+
+?>
