84f4f6cb84fbe99c80195f66681c04b1697f1e02
SachinthaniManthilaka <139526328+SachinthaniManthilaka@users.noreply.github.com>
2024-12-12 19:40:31 +0000

    username changed and again added the same

--- public/functions.php
+++ public/functions.php
@@ -73,7 +73,7 @@ function printTable1($connect,$name,$contact_no,$email,$message){
 //insert data into table
     function addData($connect,$name,$contact_no,$email,$message){
         try {
-            $sql = "INSERT INTO $contactus VALUES ('$name','$contact_no','$email','$message')";
+            $sql = "INSERT INTO contactus VALUES ('$name','$contact_no','$email','$message')";
     
         $result = mysqli_query($connect,$sql);
         if ($result) {
