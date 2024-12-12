6ac8bfac993ea9d7fd71ba43d49faef7abefaf0e
SachinthaniManthilaka <139526328+SachinthaniManthilaka@users.noreply.github.com>
2024-12-12 19:14:02 +0000

    changed table column datas

--- public/functions.php
+++ public/functions.php
@@ -73,7 +73,7 @@ function printTable1($connect,$name,$contact_no,$email,$message){
 //insert data into table
     function addData($connect,$contactus,$name,$contact_no,$email,$message){
         try {
-            $sql = "INSERT INTO $contactus VALUES ('$reg','$name','$age','$course')";
+            $sql = "INSERT INTO $contactus VALUES ('$name','$contact_no','$email','$message')";
     
         $result = mysqli_query($connect,$sql);
         if ($result) {
