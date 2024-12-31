28ae3ec2aca73ba432ea6f65590b14f59bcc55ba
Sachinthathani <4567sachimanthilaka@gmail.com>
2025-01-01 00:27:38 +0530

    created redmi mobile php page

---
+++ models_redmi.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redmi</title>
+    <link rel="stylesheet" href="CSS/New.css">
+	<link href="bootstrap-5.3.3-dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="head-c">
+	<div class="row">
+    <script src="bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
+    <header>
+      <div class="header">
+        <div class="container">
+        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+          <div class="full">
+              <div class="center-desk">
+                  <div class="logo">
+                      <h2><b>The Mobile Phone</b></h2>
+                  </div>
+              </div>
+          </div>
+        </div>
+
+        <!--navbar-->
+        <div class="nav1">
+          <!--<nav class="navbar navbar-expand-lg bg-body-tertiary">-->
+            <nav class="navbar navbar-expand-lg custom-navbar">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#"></a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+</button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="about.php">About</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Brand</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact Us</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="singup page.php">Log In</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Models
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="models_sam.php">Samsung</a></li>
+                      <li><a class="dropdown-item" href="models_iphone.php">iPhone</a></li>
+                      <!--<li><hr class="dropdown-divider"></li>-->
+                      <li><a class="dropdown-item" href="models_redmi.php">Redmi</a></li>
+</ul>
+                  </li>
+                </ul>
+                <form class="d-flex" role="search">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+              </div>
+</div>
+</nav>
+          </div>
+         </div>
+
+    <br>
+    <hr>
+</div>
+<h4>Samsung</h4>
+
+<!-- Products Group-->
+<div class="row-products">
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="products card h-100">
+      <img src="Img/samsung_S24.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Samsung S24</h5>
+        <p class="card-text">Display: 6.1" Dynamic AMOLED 2X, 120Hz, FHD+<br>
+          Chipset: Snapdragon 8 Gen 2 (USA/China) or Exynos 2200 (Global)<br>
+          Storage: 128 GB or 256 GB (no microSD)<br>
+          Cameras:<br>
+          Rear: 50 MP (main), 12 MP (ultrawide), 10 MP (telephoto, 3x zoom)<br>
+          Front: 12 MP<br>
+          Battery: 3,900 mAh, 25W fast charging<br>
+          Other: IP68 water resistance, 5G, no headphone jack, stereo speakers<br>
+          <br>
+          Rs.175000.00</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="products card h-100">
+      <img src="Img/samsung_S22n.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Samsung S22</h5>
+        <p class="card-text">Display: 6.1" Dynamic AMOLED 2X, 120Hz, FHD+<br>
+          Chipset: Snapdragon 8 Gen 1 (USA/China) or Exynos 2200 (Global)<br>
+          Storage: 128 GB or 256 GB (no microSD)<br>
+          Cameras:<br>
+          Rear: 50 MP (main), 12 MP (ultrawide), 10 MP (telephoto, 3x zoom)<br>
+          Front: 10 MP<br>
+          Battery: 3,700 mAh, 25W fast charging<br>
+          Other: IP68 water resistance, 5G, stereo speakers, no headphone jack<br><br>
+          Rs.139000.00</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="products card h-100">
+      <img src="galaxy-s24-ultra-1.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Galaxy S24 Ultra</h5>
+        <p class="card-text">6.8" QHD+ Dynamic AMOLED display<br>
+Snapdragon 8 Gen 3 processor<br>
+Up to 12GB RAM and 1TB storage<br>
+200MP main camera with enhanced AI features<br>
+5000mAh battery<br>
+S Pen support</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="products card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Galaxy S24</h5>
+        <p class="card-text">6.2" FHD+ Dynamic AMOLED display<br>
+Snapdragon 8 Gen 3 processor<br>
+8GB RAM and up to 256GB storage<br>
+50MP main camera<br>
+4000mAh battery</p>
+      </div>
+      <div class="card-footer">
+        <a href="S24.jpg" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="products card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Galaxy Z Fold5</h5>
+        <p class="card-text">7.6" main display when unfolded<br>
+6.2" cover display<br>
+Snapdragon 8 Gen 2 processor<br>
+12GB RAM<br>
+Improved hinge design.</p>
+      </div>
+      <div class="card-footer">
+        <a href="Z fold5.jpg" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="products card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Galaxy Z Flip5</h5>
+        <p class="card-text">6.7" main display when unfolded<br>
+3.4" cover display<br>
+Snapdragon 8 Gen 2 processor<br>
+8GB RAM.</p>
+      </div>
+      <div class="card-footer">
+        <a href="Samsung-Galaxy-Z-Flip-5.jpg" class="btn btn-primary">Buy Now</a>
+</div>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="row-products">
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="products card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Galaxy A55 5Gs</h5>
+        <p class="card-text">6.6" Super AMOLED display<br>
+Exynos 1480 processor<br>
+Up to 8GB RAM, 256GB storage<br>
+50MP main camera<br>
+5000mAh battery<br>
+IP67 water resistance.</p>
+      </div>
+      <div class="card-footer">
+        <a href="Samsung Galaxy A55-500x500.jpj" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="products card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Galaxy A35 5G</h5>
+        <p class="card-text">6.6" Super AMOLED display<br>
+Exynos 1380 processor<br>
+6/8GB RAM, up to 256GB storage<br>
+48MP main camera<br>
+5000mAh battery</p>
+      </div>
+      <div class="card-footer">
+        <a href="A35.jpj" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+</div>
+  <div class="col">
+    <div class="products card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Galaxy A25 5G</h5>
+        <p class="card-text">6.5" Super AMOLED display<br>
+Exynos 1280 processor<br>
+6/8GB RAM, 128GB storage<br>
+50MP main camera<br>
+5000mAh battery</p>
+      </div>
+      <div class="card-footer">
+        <a href="Samsung-Galaxy-A25-5G-Fantasy-Blue.jpg" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="products card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Galaxy M55 5G</h5>
+        <p class="card-text">6.7" Super AMOLED display<br>
+Snapdragon 7 Gen 1 processor<br>
+Up to 8GB RAM, 256GB storage<br>
+50MP main camera<br>
+5000mAh battery</p>
+      </div>
+      <div class="card-footer">
+        <a href="MBPSSM-M556BLGASLK-S-1--1730187495.jpg" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="products card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Galaxy M35 5G</h5>
+        <p class="card-text">6.6" Super AMOLED display<br>
+MediaTek processor<br>
+6GB RAM, 128GB storage<br>
+50MP main camera<br>
+6000mAh battery.</p>
+      </div>
+      <div class="card-footer">
+        <a href="Samsung-Galaxy-M35-8GB-RAM-256GB-Price-In-Sri-Lanka.jpg" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="products card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Galaxy F55 5G</h5>
+        <p class="card-text">Similar specs to A55 with market-specific variations<br>
+Online exclusive availability<br>
+Competitive pricing</p>
+      </div>
+      <div class="card-footer">
+        <a href="samsung-galaxy-f55.jpg" class="btn btn-primary">Buy Now</a>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+</body>
+</html>
