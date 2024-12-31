0817d7af945f4ce4c5351a235b2b1340ea93a226
Sandun Senarath <139609887+SandunSenarath@users.noreply.github.com>
2024-12-31 11:37:10 +0000

    update models samsung

--- models_sam.php
+++ models_sam.php
@@ -123,7 +123,7 @@
   </div>
   <div class="col">
     <div class="products card h-100">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
